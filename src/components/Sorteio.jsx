0f9f699496ce646ca32df912f6d098f878c26168
Jonas Nutels <jonas.nutels@tceal.tc.br>
2023-12-22 09:58:43 -0300

    mudanca radical

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -1,31 +1,77 @@
 import styles from './Sorteio.module.css';
-import coracaoIcon from '../assets/heart.svg';
-function Sorteio() {
-  const HeartContainer = () => {
-    const hearts = Array.from({ length: 100 }, (_, index) => index + 1);
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../userContext';
 
-    return (
-      <div className={styles['heart-container']}>
-        {hearts.map((number) => (
-          <div key={number} className={styles.heart}>
-            <span className={styles['heart-number']}>{number}</span>
-          </div>
-        ))}
-      </div>
-    );
+function Sorteio() {
+  const { getNumeros, numeros, updateNumeroStatus } = useContext(UserContext);
+  const [nome, setNome] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [selectedNumeroId, setSelectedNumeroId] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    getNumeros();
+  }, []);
+
+  const handleNumeroSelect = async (selectedNumero) => {
+    setSelectedNumeroId(selectedNumero.id);
+    setModalOpen(true);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await updateNumeroStatus(selectedNumeroId, 'Indisponivel', nome, telefone);
+    getNumeros();
+    console.log('ID', selectedNumeroId, 'Dados do formulário:', {
+      nome,
+      telefone,
+    });
+
+    setModalOpen(false);
+    setNome('');
+    setTelefone('');
+  };
+
   return (
     <>
       <div className={styles.container}>
-        <img className={styles.heartImg} src={coracaoIcon} alt="" width={100} />
-        <div className={styles.titulo}>
-          <h1>Chá Rifa da</h1>
-          <span className={styles.nomeZoe}>Zoe</span>
-        </div>
-        <img className={styles.heartImg} src={coracaoIcon} alt="" width={100} />
-      </div>
-      <div className={styles.painel}>
-        <HeartContainer />
+        <h1>Sorteio</h1>
+        <ul>
+          {numeros.map((numero) => (
+            <li key={numero.id} onClick={() => handleNumeroSelect(numero)}>
+              Número: {numero.numero}, Status:
+              <span className={styles[numero.status]}>{numero.status}</span>
+              {selectedNumeroId === numero.id && modalOpen && (
+                <>
+                  <form onSubmit={handleSubmit}>
+                    <label>
+                      Nome:
+                      <input
+                        type="text"
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                      />
+                    </label>
+                    <label>
+                      Telefone:
+                      <input
+                        type="tel"
+                        value={telefone}
+                        onChange={(e) => setTelefone(e.target.value)}
+                        pattern="\d{11}"
+                        title="Ex: 82999999999"
+                        required
+                      />
+                    </label>
+
+                    <button type="submit">Enviar</button>
+                  </form>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
